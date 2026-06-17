d284eea830c203e6bee615e1a765b47b2a3f1d8a
HiddenCirno <2301697863@qq.com>
2026-06-17 13:10:09 +0800

    代码提交

---
+++ PluginsInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoWorkout
+{
+    public class PluginsInfo
+    {
+        public const string GUID = "spark.hiddenhiraigi.autoworkout";
+        public const string NAME = "AutoWorkout";
+        public const string VERSION = "1.0.0";
+    }
+}
---
+++ QTEPatch.cs
@@ -0,0 +1,40 @@
+﻿using EFT.Hideout;
+using HarmonyLib;
+
+namespace AutoWorkout
+{
+    public static class QTEStateManager
+    {
+        public static bool HasPlayerClicked = false;
+    }
+
+    [HarmonyPatch(typeof(ShrinkingCircleQTE), nameof(ShrinkingCircleQTE.StartAction))]
+    public class QTEResetPatch
+    {
+        static void Prefix()
+        {
+            QTEStateManager.HasPlayerClicked = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ShrinkingCircleQTE), nameof(ShrinkingCircleQTE.method_1))]
+    public class QTEClickTrackerPatch
+    {
+        static void Prefix()
+        {
+            QTEStateManager.HasPlayerClicked = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ShrinkingCircleQTE), nameof(ShrinkingCircleQTE.method_3))]
+    public class AutoGymTimeoutPatch
+    {
+        static void Prefix(ref bool success)
+        {
+            if (success == false && QTEStateManager.HasPlayerClicked == false)
+            {
+                success = true;
+            }
+        }
+    }
+}
