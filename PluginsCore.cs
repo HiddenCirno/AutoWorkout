b4bf146217cfe4a4750a7e953ec9e2a0f590d866
HiddenCirno <2301697863@qq.com>
2026-08-11 16:19:53 +0800

    4.1 upgrade

---
+++ PluginsCore.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace AutoWorkout
+{
+    [BepInPlugin(PluginsInfo.GUID, PluginsInfo.NAME, PluginsInfo.VERSION)]
+    public class PluginsCore : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony(PluginsInfo.GUID);
+            harmony.PatchAll();
+        }
+        public void Start()
+        {
+        }
+        public void Update()
+        {
+        }
+    }
+}
